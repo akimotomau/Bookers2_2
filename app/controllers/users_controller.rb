846283f2e38bedb4962f529627609e1c8b1ca573
akimotomau <87314723+akimotomau@users.noreply.github.com>
2024-02-12 22:08:30 +0000

    almost done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,16 +10,21 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find([:id])
+    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find([:id])
-    if @user.update
-      flassh[:notice] = "You have updated user successfully."
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
     else 
       render edit_user_path
     end
   end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name,:introduction,:profaile_imagge)
+  end  
 end

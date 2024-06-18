747ee44e418592915cc630220a87ebf3f3c24eb4
EC2 Default User <ec2-user@ip-172-31-27-203.ap-northeast-1.compute.internal>
2024-06-18 15:30:53 +0000

    add follower asociation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follwers, through: reverse_of_relationships, sorce: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :follweings, through: relationships, sorce: :followed
 
   has_one_attached :profile_image
 
